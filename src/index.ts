d3e20e823cedc278f865532f36e8208ced4bc8ad
Nhac Duy Le <nlduy@codix.eu>
2024-10-23 20:30:18 +0700

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,31 @@ import express, { Request, Response } from 'express';
 import FirecrawlApp, { CrawlParams, CrawlStatusResponse } from '@mendable/firecrawl-js';
 
 const app = express();
-const appFirecrawl  = new FirecrawlApp({apiKey: "fc-6771a06bf4de4e1599d8fce547334A9e"});
+const appFirecrawl  = new FirecrawlApp({apiKey: "fc-6771a06bf4de4e1599d8fce547334a9e"});
 const port = 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // Simple GET route
-app.get('/api', async (req: Request, res: Response) => {
-// Scrape a website
-const scrapeResponse = await appFirecrawl.scrapeUrl('https://firecrawl.dev', {
+app.get('/api', async (req: Request, res: Response) => {  
+    res.send('Hello from the Node Firecrawl API!');
+});
+
+app.get('/api/scrape', async (req: Request, res: Response) => {
+  // Scrape a website
+  const scrapeResponse = await appFirecrawl.scrapeUrl('https://firecrawl.dev', {
     formats: ['markdown', 'html'],
   });
-  
+
   if (scrapeResponse) {
-    console.log(scrapeResponse)
+    res.send(scrapeResponse);
+  }else{
+    res.send('Failed to scrape the website');
   }
-  
+});
+
+app.get('/api/crawl',  async (req: Request, res: Response) => {
   // Crawl a website
   const crawlResponse = await appFirecrawl.crawlUrl('https://firecrawl.dev', {
     limit: 100,
@@ -26,12 +34,30 @@ const scrapeResponse = await appFirecrawl.scrapeUrl('https://firecrawl.dev', {
       formats: ['markdown', 'html'],
     }
   } as CrawlParams, 30) as CrawlStatusResponse;
-  
+
   if (crawlResponse) {
-    console.log(crawlResponse)
-  }    
-    res.send('Hello from the Node Firecrawl API!');
+    res.send(crawlResponse)
+  }else{
+    res.send('Failed to crawl the website');
+  }
 });
+
+app.get('/api/crawl_cdx',  async (req: Request, res: Response) => {
+  // Crawl a website
+  const crawlResponse = await appFirecrawl.crawlUrl('https://wiki.codix.eu/wiki/index.php?title=CodiX_Group_Intranet_Main_Page', {
+    limit: 100,
+    scrapeOptions: {
+      formats: ['markdown', 'html'],
+    }
+  } as CrawlParams, 30) as CrawlStatusResponse;
+
+  if (crawlResponse) {
+    res.send(crawlResponse)
+  }else{
+    res.send('Failed to crawl the website');
+  }
+});
+
 
 // A sample POST route
 app.post('/api/data', (req: Request, res: Response) => {
